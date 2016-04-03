604bdcb97e1594870afe8d5df2dbd1d8845fa736
Stella Gao <stella.gao.89@gmail.com>
2016-04-03 00:20:12 -0400

    Update Java_Static_Initializer_Block.java

--- Introduction/Java_Static_Initializer_Block.java
+++ Introduction/Java_Static_Initializer_Block.java
@@ -14,7 +14,7 @@ public class Solution {
 		B = scanner.nextInt();
 		H = scanner.nextInt();
 		if (B>0 && H>0) flag = true;
-        else			System.out.println("java.lang.Exception: Breadth and height must be positive");
+        	else		System.out.println("java.lang.Exception: Breadth and height must be positive");
 	}
 	
 	public static void main(String[] args){
