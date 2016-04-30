30c77d49d25bd9cc8b30ec3a124eed48a5dfc4f7
Stella Gao <stella.gao.89@gmail.com>
2016-04-29 23:17:17 -0400

    Update Java Regex.java

--- Strings/Java Regex.java
+++ Strings/Java Regex.java
@@ -1,7 +1,18 @@
+// Method 1 :
 class myRegex{
     String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
     public String pattern = zeroTo255 + "." + zeroTo255 + "." + zeroTo255 + "." + zeroTo255;
 }
+
+// Method 2:
+class myRegex{
+    public static final String pattern = 
+		"^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+}
+
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
